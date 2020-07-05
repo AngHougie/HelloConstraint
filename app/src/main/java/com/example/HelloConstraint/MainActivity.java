6391ab1fe743d1afcfcaa2ec6e664974801af2dc
AngHougie <angh-wm19@student.tarc.edu.my>
2020-07-05 16:33:35 +0800

    commit by anghougie

--- app/src/main/java/com/example/HelloConstraint/MainActivity.java
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.practical_2_part_a;
+package com.example.HelloConstraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    View v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        if(mCount%2==0)
+            view.setBackgroundColor(getResources().getColor(R.color.even));
+        if(mCount%2!=0)
+            view.setBackgroundColor(getResources().getColor(R.color.odd));
+        v=findViewById(R.id.button_zero);
+        v.setBackgroundColor(getResources().getColor(R.color.green));
     }
+
+    public void countZero(View view) {
+        mCount=0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        view.setBackgroundColor(getResources().getColor(R.color.darker_gray));
+        v=findViewById(R.id.button_count);
+        v.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+    }
+
 }
